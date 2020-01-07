dfded908396ea6cca5a9d853c54286f43609d466
Kamil Mąkosa <kamil.makosa13@gmail.com>
2020-01-07 06:57:44 +0100

    added mongoose + login

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:rUwF5zvogQy8dB9v@cluster0-t0cfg.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const News = require('../models/news');
 const router = express.Router();
+
+
+router.all('*', (req, res, next) => {
+  if(!req.session.admin) {
+    res.redirect('login');
+
+    return;
+  }
+
+  next();
+})
 
 /* GET home page. */
 router.get('/', (req, res) => {
+  const newsData = new News({
+    title: 'Tytuł testowy',
+    description: 'Oppis'
+  });
+
+  newsData.save((err) => {
+    console.log(err);
+  })
+
   res.render('admin', { title: 'Admin' });
 });
 
